5397528872d095b2cce0dbd40953bbacc84ef3e6
Amirah Harb <harbamirah@gmail.com>
2022-02-26 04:31:01 -0400

    Separating/refactoring display function

--- client.js
+++ client.js
@@ -7,7 +7,7 @@ const messageContainer = document.getElementById('message-cont')
 //receiving message from server
 socket.on('sent-message', message => {
     console.log(message)
-    displayMessage(message, "recipient")
+    displayMessageLeft(message)
 })
 
 //sending message to server
@@ -18,39 +18,42 @@ sendForm.addEventListener('submit', e => {
     socket.emit('send-message', message)
 
     //displaying message to sender
-    displayMessage(message, "sender")
+    displayMessageRight(message)
+
     sendInput.value = ''
 })
 
-function displayMessage(message, recipient){
+
+function displayMessageRight(message){
     const messageElement = document.createElement('div')
     
-
     messageElement.style.maxWidth = "60%"
     messageElement.innerText = message
     messageElement.style.fontFamily = "sans-serif"
     messageElement.style.padding = "2px 10px 2px 10px"
     messageElement.style.marginBottom = "10px"
 
-    if(recipient == 'sender'){
-        messageElement.style.marginLeft = "auto"
-        messageElement.style.backgroundColor = "#9795BC"
-        messageElement.style.borderRadius = "10px 3px 10px 10px "
-        messageElement.style.marginRight = "10px"
-    }
-
-    if(recipient == 'recipient'){
-        messageElement.style.marginRight = "auto"
-        messageElement.style.backgroundColor = "#e0e1dd"
-        messageElement.style.borderRadius = "3px 10px 10px 10px"
-        messageElement.style.marginLeft = "10px"
-    }
+    messageElement.style.marginLeft = "auto"
+    messageElement.style.backgroundColor = "#9795BC"
+    messageElement.style.borderRadius = "10px 3px 10px 10px "
+    messageElement.style.marginRight = "10px"
 
     messageContainer.append(messageElement)
 }
 
+function displayMessageLeft(message){
+    const messageElement = document.createElement('div')
+    
+    messageElement.style.maxWidth = "60%"
+    messageElement.innerText = message
+    messageElement.style.fontFamily = "sans-serif"
+    messageElement.style.padding = "2px 10px 2px 10px"
+    messageElement.style.marginBottom = "10px"
 
+    messageElement.style.marginRight = "auto"
+    messageElement.style.backgroundColor = "#e0e1dd"
+    messageElement.style.borderRadius = "3px 10px 10px 10px"
+    messageElement.style.marginLeft = "10px"
 
-
-
-
+    messageContainer.append(messageElement)
+}
